341e660071bb383c11ef030428c7660f132c49f8
Grant Steuart <gmsteuart@gmail.com>
2022-05-14 17:53:08 -0500

    fix: adder typing

--- example.js
+++ example.js
@@ -1,17 +1,12 @@
 import { casetest } from './casetest.js'
 
 const lib = {}
-lib.adder =(a,b)=> [0, a+b] // result type
-
-const argsConverter = (s) => {
-    const n = Number(s)
-    return isFinite(n) && !isNaN(n) ? n : s
-}
+lib.adder =(a,b)=> [0, Number(a) + Number(b)] // result type
 
 casetest('./case', (t, {func,args,want})=>{
     // map it onto your lib
     // e.g. fromHex
-    let [err, ret] = lib[func](...args.map(argsConverter))
+    let [err, ret] = lib[func](...args)
     t.ok(!err)
     t.equal(ret, want[1])
 })
